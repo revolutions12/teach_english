514dc7566fe3ca369ee2e60dbfdc91454d105529
Charles Peterson <nowled@operamail.com>
2012-02-18 23:31:31 +0900

    Initial  commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeachEnglish::Application.config.secret_token = 'f2b50ad4a0bec047b286d6766521d50007d4e238bfefaab2d14ab089e2656d18871f4e0ee53eca17fe2d8744eaa5fb30b1120b6ba4855c19f77f8c74887c293d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TeachEnglish::Application.config.session_store :cookie_store, key: '_teach_english_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TeachEnglish::Application.config.session_store :active_record_store
